82b92198b5f69e35769734b8666902ba3110a37f
anonymous-oracle <suhass149@gmail.com>
2021-03-29 23:27:14 +0530

    section 5 chapter 6

--- ForLoop/src/App.java
+++ ForLoop/src/App.java
@@ -7,9 +7,27 @@ public class App {
         System.out.println("10000 at 2% interest = " + calculateInterest(10000, 0.02));
         for (int i = 1; i <= 10; i++) {
             System.out.println(calculateInterest(10000, i + 1));
-    
+
         }
 
+        System.out.println("*************************************************");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(calculateInterest(10000, i + 1));
+
+        }
+
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
