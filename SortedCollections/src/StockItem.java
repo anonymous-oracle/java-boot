aa60b0d184e3e3cc99e41bb83c98f9ec8d2c02ed
anonymous-oracle <suhass149@gmail.com>
2021-05-16 23:17:43 +0530

    section 12 chapter 23

--- SortedCollections/src/StockItem.java
+++ SortedCollections/src/StockItem.java
@@ -1,12 +1,17 @@
-public class StockItem implements Comparable { // allows to implement compareTo() method
+public class StockItem implements Comparable<StockItem> { // allows to implement compareTo() method
     private final String name;
     private double price;
-    private int quantityStock = 0; // can be initialized later
+    // private int quantityStock = 0; // can be initialized later
+    private int quantityStock;
 
     public StockItem(String name, double price) {
+        this(name, price, 0);
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
         this.name = name;
         this.price = price;
-        this.quantityStock = 0; // or here (but you wouldn't normally do both)
+        this.quantityStock = quantityStock;
     }
 
     public String getName() {
@@ -31,14 +36,45 @@ public class StockItem implements Comparable { // allows to implement compareTo(
         int newQuantity = this.quantityStock + quantity;
         if (newQuantity >= 0) {
             this.quantityStock = newQuantity;
-        
+
         }
     }
 
     @Override
-    public int compareTo(Object o) {
-        // TODO Auto-generated method stub
-        return 0;
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals()");
+        if (obj == this) {
+            return true;
+        }
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
     }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("ENtering StockItem.compareTo");
+        if (this == o) {
+            return 0;
+        }
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : price " + this.price;
+    }
+
+    
 
 }
---
+++ SortedCollections/src/StockList.java
@@ -0,0 +1,56 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new LinkedHashMap<>(); // linked hash map orders items in alphabetical order
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // check if there is already a nonzero quantity of the item
+            if (inStock != item) { // if there's already an item adjust the quantity
+                item.adjustStock(inStock.getQuantityStock());
+            }
+            list.put(item.getName(), item);
+            return item.getQuantityStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+        if ((inStock != null) && (inStock.getQuantityStock() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem getStockItem(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> getItems() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List:\n";
+        double totalCost = 0.0;
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+            double itemValue = stockItem.getPrice() * stockItem.getQuantityStock();
+            s = s + stockItem + ", There are " + stockItem.getQuantityStock() + " in stock. Value of items: ";
+            s = s + itemValue + "\n";
+            totalCost += itemValue;
+        }
+        return s + "Total stock value " + totalCost;
+
+    }
+}
