1856099ba8cab226b8e91a22b3d17d77fc4688e6
anonymous-oracle <suhass149@gmail.com>
2021-06-02 23:16:41 +0530

    section 14 chapter 14

--- ByteStream/src/com/company/Main.java
+++ ByteStream/src/com/company/Main.java
@@ -1,28 +1,33 @@
 package com.company;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
-public class Challenge {
+public class Main {
     private static String characters = "";
     private static String scrambledString = "";
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = null;
+        DataOutputStream dos = null;
         try {
-            bw = new BufferedWriter(new FileWriter("regex.txt"));
+            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("regex.dat")));
             for (int i = 0; i < 10; i++) {
                 initCharacterString();
                 String text = getScrambledString(50, 7);
-                bw.write(text);
-//                bw.write("\n");
+                byte[] bytes = text.getBytes();
+//                for (byte aByte : bytes) {
+//                    dos.write();
+//                }
+                dos.write(bytes);
+//                dos.write("\n");
             }
-            bw.close();
+            dos.close();
         } catch (IOException e) {
-            if (bw != null) {
-                bw.close();
+            if (dos != null) {
+                dos.close();
             }
             e.printStackTrace();
         }
